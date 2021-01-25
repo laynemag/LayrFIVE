1a34b3f88a67ad2b453ac191582baed2501114e2
Andrea Myers <andreatmyers5@gmail.com>
2021-01-25 14:40:19 -0500

    file structure

---
+++ routes/homepage.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/homepage', (req, res) => {
+    
+    res.render('homepage')
+})
+
+
+
+
+
+module.exports = router;
