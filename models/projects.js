4a164df72f5f63576a0a3b72ba9fabb2b138c2e0
Adam MacKinnon <mackinnon.adam@gmail.com>
2021-02-02 14:02:58 -0500

    re added database

--- models/projects.js
+++ models/projects.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       models.users.hasMany(models.projects, {foreignKey: "id"})
+      models.comment.belongsTo(models.projects, {foreignKey: "projectID"})
     }
   };
   projects.init({
--- public/js/comments.js
+++ public/js/comments.js
@@ -2,9 +2,20 @@
 const submitButton = document.getElementById('submitButton');
 const commentBox = document.getElementById('commentBox');
 
-submitButton.addEventListener('click', (e) => {
+submitButton.addEventListener('click', async (e) => {
     e.preventDefault();
     console.log('Comment Submitted!');
     console.log(commentBox.value);
-
+    try{
+    let response = await fetch("/project", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            commentBox: commentBox.value
+        })
+    })
+    }
+    catch(error){
+        console.log(error);
+    }
 })
