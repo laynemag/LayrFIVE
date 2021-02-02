55550b2a98742713c96cdd8ab89ed4c1fe014d5f
Ally Deeter <allydeeter6639@gmail.com>
2021-02-02 14:16:31 -0800

    fixing errors with comments

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,13 +1,19 @@
 
 const submitButton = document.getElementById('submitButton');
 const commentBox = document.getElementById('commentBox');
+let userID = document.getElementById('userID')
+let projectID = document.getElementById('projectID')
+
+console.log(projectID);
 
 submitButton.addEventListener('click', async (e) => {
     e.preventDefault();
     console.log('Comment Submitted!');
     console.log(commentBox.value);
+    console.log(parseInt(projectID.value));
+    console.log(parseInt(userID.value));
     try{
-    let response = await fetch("/project", {
+    let response = await fetch(`/project/${parseInt(projectID.value)}/${parseInt(userID.value)}`, {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
