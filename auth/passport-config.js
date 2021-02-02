a2c435f041fef19c8be206107c9bf174b6d5870e
Layne Magnuson <laynemag@gmail.com>
2021-02-02 09:40:21 -0600

    added features and things

--- auth/passport-config.js
+++ auth/passport-config.js
@@ -7,7 +7,7 @@ const init = (passport) => {
     passport.use( new LocalStrategy(async(username, password, done) =>{
         
         try{
-        await db.users.findAll({where: {username: username}})
+        let records = await db.users.findAll({where: {username: username}})
             
             if( records  != null){
                     let record = records[0];
@@ -33,7 +33,7 @@ const init = (passport) => {
         
     }
     catch (err){
-        console.log("error in config");
+        console.log(err);
         done(null,false)
     }
 }))
