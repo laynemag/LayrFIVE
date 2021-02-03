55e4924c658ffe5fe391f7f4a2028a7b60fd2e47
Ally Deeter <allydeeter6639@gmail.com>
2021-02-03 10:01:07 -0800

    submissions redirect

--- public/js/registration.js
+++ public/js/registration.js
@@ -6,34 +6,35 @@ var imageGithub;
 
 
 submitButton.addEventListener('click', async (e) => {
-        e.preventDefault()
-        try{
-          let url = `https://api.github.com/users/${githubUsername.value}`
-          let response = await fetch(url)
-          let result = await response.json()
+    e.preventDefault()
+    try{
+      let url = `https://api.github.com/users/${githubUsername.value}`
+      let response = await fetch(url)
+      let result = await response.json()
 
-          if (result.avatar_url){
-            imageGithub = result.avatar_url
-          } else {
-            imageGithub = '/images/avatar2.jpg'
-          }
+      if (result.avatar_url){
+        imageGithub = result.avatar_url
+      } else {
+        imageGithub = '/images/avatar2.jpg'
+      }
 
-          let responseBack = await fetch('/registration', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              username: username.value,
-              password: password.value,
-              githubUsername: githubUsername.value,
-              imageurl: imageGithub
-            })
-          })
-          
-        }
+      let responseBack = await fetch('/registration', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          username: username.value,
+          password: password.value,
+          githubUsername: githubUsername.value,
+          imageurl: imageGithub
+        })
+      })
+      console.log(responseBack);
+      
+    }
 
-        catch(error){
-          console.log(error);
-        }
+    catch(error){
+      console.log(error);
+    }
 
-        window.location.assign('/index')
+    window.location.assign('/index')
 })
